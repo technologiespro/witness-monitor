a8023b66f99f74aba2b9f2d2b89b40c691eb0656
technolog <technologies.bro@gmail.com>
2020-08-02 15:34:32 +0300

    provider:getPrices

---
+++ modules/provider.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+// bitshares asset: coinpaprika ticker
+
+
+
+
+class priceProvider {
+    constructor(config) {
+        this.config = config
+    }
+
+    async getPrices() {
+        let result = null
+        const apiUrl = this.config.apiUrl + Object.values(this.config.currency).join()
+        try {
+            result = (await axios.get(apiUrl)).data
+        } catch(e) {
+
+        }
+        return result
+    }
+}
+
+
+module.exports = priceProvider;
--- providers/coinpaprika.js
+++ providers/coinpaprika.js
@@ -1,11 +1,15 @@
-// bitshares asset: coinpaprika ticker
-const currency = {
-    "EUR": "EUR",
-    "USD": "USD",
-    "RUBLE": "RUB",
-    "CNY": "CNY"
+const provider = require('../modules/provider')
+
+const paprika = new provider({
+    apiUrl: "https://api.coinpaprika.com/v1/tickers/bts-bitshares?quotes=",
+    currency: {
+        "EUR": "EUR",
+        "USD": "USD",
+        "RUBLE": "RUB",
+        "CNY": "CNY"
+    },
+})
+
+async function getPrices() {
+    return await paprika.getPrices()
 }
-
-const apiUrl = "https://api.coinpaprika.com/v1/tickers/bts-bitshares?quotes=" + Object.values(currency).join()
-
-console.log(apiUrl)
