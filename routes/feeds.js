994b98dc86716f9a1e36402f217b1c985bc4661b
technolog <technologies.bro@gmail.com>
2020-08-02 16:01:49 +0300

    scheduleJob

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const Paprika = require('../providers/coinpaprika');
-
+const express = require('express')
+const router = express.Router()
+const scheduler = require("node-schedule")
+const Paprika = require('../providers/coinpaprika')
 const paprika = new Paprika()
+
+scheduler.scheduleJob("1 */1 * * * *", async () => {
+    console.log(await paprika.getPrices())
+});
+
 
 /* GET home page. */
 router.get('/prices', async function(req, res, next) {
