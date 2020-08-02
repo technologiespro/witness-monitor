e4f5040a7a3c458d9efacd3cdaabe7e6510f2e2c
technolog <technologies.bro@gmail.com>
2020-08-02 20:29:15 +0300

    paprika currency

--- providers/coinpaprika.js
+++ providers/coinpaprika.js
@@ -1,26 +1,26 @@
 const provider = require('../modules/provider')
+const currency = {
+    "EUR": "EUR",
+    "RUBLE": "RUB",
+}
 
 const providerPaprika = new provider({
     apiUrl: "https://api.coinpaprika.com/v1/tickers/bts-bitshares?quotes=",
-    currency: {
-        "EUR": "EUR",
-        "RUBLE": "RUB",
-    },
+    currency: currency
 })
 
 class paprika {
     async getPrices() {
         const quotes = (await providerPaprika.getPrices()).quotes
-        let result = {
-            "EUR": {
-                price: quotes.EUR.price.toFixed(6),
+        let result = {}
+        let qAssets = Object.keys(currency)
+        for (let i = 0; i < qAssets.length; i++) {
+            result[qAssets[i]] = {
+                price: quotes[currency[qAssets[i]]].price,
                 cer: 0,
-            },
-            "RUBLE": {
-                price: quotes.RUB.price.toFixed(6),
-                cer: 0,
-            },
+            }
         }
+
         return result
     }
 }
