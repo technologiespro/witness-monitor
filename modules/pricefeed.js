ec9bf5167b6db4c177947c3ce29b54227983e61e
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-10-24 19:48:34 +0300

    options.config

--- modules/pricefeed.js
+++ modules/pricefeed.js
@@ -1,13 +1,24 @@
 class feeds {
     constructor(options) {
-        this.options = options
+        this.options = options;
+        this.assets = {};
+        this.latestFeeds = {};
     }
 
     async init() {
-        this.account = new this.options.BitSharesInstance(this.options.producer.name, this.options.producer.key);
-        this.feeder = await this.options.BitSharesInstance.accounts[this.options.producer.name];
+        this.account = new this.options.BitSharesInstance(this.options.config.producer.name, this.options.config.producer.key);
+        this.feeder = await this.options.BitSharesInstance.accounts[this.options.config.producer.name];
         console.log('account', this.feeder.id, this.feeder.name);
+    }
 
+    async feelPrices() {
+        this.assets[CONFIG.coreAsset] = (await BitShares.assets[CONFIG.coreAsset]);
+        let feedAssets = Object.keys(CONFIG.priceFeeds.assets);
+        this.latestFeeds = await paprika.getPrices();
+        for (let i = 0; i < feedAssets.length; i++) {
+            assets[feedAssets[i]] = (await BitShares.assets[feedAssets[i]]);
+            latestFeeds[feedAssets[i]].cer = (latestFeeds[feedAssets[i]].price + (latestFeeds[feedAssets[i]].price * 0.08)).toFixed(8) * 1;
+        }
     }
 
 
