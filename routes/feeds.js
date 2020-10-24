7c044937f6fc62c84e6496f19f22a4f7f456f591
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2020-10-24 18:36:25 +0300

    feeds

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const Paprika = require('../providers/coinpaprika')
-const paprika = new Paprika()
+const express = require('express');
+const router = express.Router();
+const Paprika = require('../providers/coinpaprika');
+const paprika = new Paprika();
 
 /* GET home page. */
 router.get('/prices', async function(req, res, next) {
