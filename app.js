436a6163c5d72c7b563621bfbf1354989257c839
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2021-12-31 12:42:17 +0300

    dev log off

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+//const logger = require('morgan'); //dev log
 
 const jsonFile = require('jsonfile');
 const config = jsonFile.readFileSync('./config.json');
@@ -18,7 +18,7 @@ app.use(helmet());
 
 app.disable("x-powered-by");
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
