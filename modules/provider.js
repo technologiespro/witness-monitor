3c8ed33f490bb1067c5a83d4dabfcb99b2a71bbd
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-11-17 13:25:49 +0300

    disable silver & cny

--- modules/provider.js
+++ modules/provider.js
@@ -5,13 +5,20 @@ class priceProvider {
         this.config = config
     }
 
-    async getPrices() {
+    async getPrices(currency = {
+        "EUR": "EUR",
+        "RUBLE": "RUB",
+        "USD": "USD",
+        "CNY": "CNY",
+        "BTC": "BTC"
+    }
+    ) {
         let result = null;
-        const apiUrl = this.config.apiUrl + Object.values(this.config.currency).join();
+        const apiUrl = this.config.apiUrl + Object.values(currency).join();
         try {
             result = (await axios.get(apiUrl)).data;
-        } catch(e) {
-
+        } catch (e) {
+            console.log('err:getPrices', e)
         }
         return result
     }
@@ -25,7 +32,7 @@ class priceProvider {
             );
             for (let i = 0; i < rez.data.data.length; i++) {
                 if (rez.data.data[i].cryptoAsset.name === 'USDT') {
-                    for (let j=0; j < rez.data.data[i].quoteAsset.length; j++) {
+                    for (let j = 0; j < rez.data.data[i].quoteAsset.length; j++) {
                         if (rez.data.data[i].quoteAsset[j].name === 'CNY') {
                             USD_CNY = rez.data.data[i].quoteAsset[j].price;
                             console.log(rez.data.data[i].quoteAsset[j]);
@@ -34,7 +41,7 @@ class priceProvider {
                     }
                 }
             }
-        } catch(e) {
+        } catch (e) {
 
         }
         return USD_CNY;
@@ -45,18 +52,19 @@ class priceProvider {
         const apiUrl = "https://api.coinpaprika.com/v1/tickers/xaut-tether-gold";
         try {
             result = (await axios.get(apiUrl)).data;
-        } catch(e) {
+        } catch (e) {
 
         }
         return result
     }
 
     async getPriceSILVER() {
+        //найти рабочий апи
         let result = null;
         const apiUrl = "https://api.coinpaprika.com/v1/tickers/xag-silver-spot-token";
         try {
             result = (await axios.get(apiUrl)).data;
-        } catch(e) {
+        } catch (e) {
 
         }
         return result
--- providers/coinpaprika.js
+++ providers/coinpaprika.js
@@ -8,12 +8,14 @@ const currency = {
     "RUBLE": "RUB",
     "USD": "USD",
     "CNY": "CNY",
+    "BTC": "BTC",
+
     "JPY": "JPY",
     "GBP": "GBP",
     "AUD": "AUD",
     "SEK": "SEK",
     "KRW": "KRW",
-    "BTC": "BTC",
+
     "MXN": "MXN",
     "CAD": "CAD",
     "CHF": "CHF",
@@ -21,29 +23,67 @@ const currency = {
 
 const providerPaprika = new provider({
     apiUrl: "https://api.coinpaprika.com/v1/tickers/bts-bitshares?quotes=",
-    currency: currency
 });
 
 
 class paprika {
     async getPrices() {
-        let quotes = (await providerPaprika.getPrices()).quotes;
+        let quotes1 = (await providerPaprika.getPrices(
+            {
+                "EUR": "EUR",
+                "RUBLE": "RUB",
+                "USD": "USD",
+            })).quotes;
+
+        let quotes2 = (await providerPaprika.getPrices(
+            {
+                "CNY": "CNY",
+                "BTC": "BTC",
+                "JPY": "JPY",
+            })).quotes;
+
+        let quotes3 = (await providerPaprika.getPrices(
+            {
+                "GBP": "GBP",
+                "AUD": "AUD",
+                "SEK": "SEK",
+            })).quotes;
+
+        let quotes4 = (await providerPaprika.getPrices(
+            {
+                "MXN": "MXN",
+                "CAD": "CAD",
+                "CHF": "CHF",
+            })).quotes;
+
+        let quotes5 = (await providerPaprika.getPrices(
+            {
+                "KRW": "KRW",
+            })).quotes;
+
+
+        let quotes = Object.assign(quotes1, quotes2, quotes3, quotes4, quotes5);
+
         let result = {};
         let qAssets = Object.keys(assets);
-        const UsdCNY = await providerPaprika.getPriceUSDCNY();
+        //const UsdCNY = await providerPaprika.getPriceUSDCNY();
         const priceGOLD = await providerPaprika.getPriceGOLD();
-        const priceSILVER = await providerPaprika.getPriceSILVER();
+        //const priceSILVER = await providerPaprika.getPriceSILVER();
         for (let i = 0; i < qAssets.length; i++) {
             if (assets[qAssets[i]].SYMBOL === 'CNY') {
-                quotes['CNY'].price = UsdCNY * quotes['USD'].price;
+               // quotes['CNY'].price = UsdCNY * quotes['USD'].price;
             }
 
             if (assets[qAssets[i]].SYMBOL === 'SILVER') {
+                /*
                 const SILVER_BTS = (priceSILVER.quotes['USD'].price / quotes['USD'].price);
                 result[qAssets[i]] = {
-                    price: (1 / SILVER_BTS).toFixed(6) * 1,
-                    cer: (1 / (SILVER_BTS + (SILVER_BTS * 0.10))).toFixed(6) * 1,
+                    price: (1 / SILVER_BTS),
+                    cer: (1 / (SILVER_BTS + (SILVER_BTS * 0.10))),
                 }
+                console.log(result[qAssets[i]])
+
+                 */
             } else if (assets[qAssets[i]].SYMBOL === 'GOLD') {
                 const GOLD_BTS = (priceGOLD.quotes['USD'].price / quotes['USD'].price);
                 result[qAssets[i]] = {
